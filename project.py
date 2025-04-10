1dd22c5ecc8aa2690fd064076f0f27f243674905
Tim Fabritius <tim.fabritius@skiff.com>
2025-04-10 15:40:09 +0300

    Update project.py

--- project.py
+++ project.py
@@ -22,7 +22,7 @@ class isr_fifo(Fifo):
 
 
 samples = isr_fifo(100, 27)  # create the improved fifo: size = 50, adc pin = pin_nr
-tmr = Piotimer(period=20, mode=Piotimer.PERIODIC, callback=samples.handler)
+tmr = Piotimer(period=10, mode=Piotimer.PERIODIC, callback=samples.handler)
 x = 0
 average = 0
 min = 65535
@@ -46,7 +46,7 @@ while True:
             if number > max:
                 max = number
 
-        if number - last < 0 and first_occurrence and number > average and peak != 0 and number != 0:
+        if number - last < 0 and first_occurrence and number > average and number != 0:
             peak = x
             first_occurrence = False
 
@@ -54,12 +54,14 @@ while True:
             first_occurrence = True
 
         if last_peak != 0 or peak != 0:
-            interval = last_peak - peak
-            interval = interval / 250
-            bpm = int(60 / interval)
-            print(bpm)
+            if peak - last_peak > 60:
+                interval = peak - last_peak
+                interval = interval / 250
+                if interval != 0:
+                    bpm = int(60 / interval)
+                    print(bpm)
 
-        time.sleep_ms(20)
+        time.sleep_ms(10)
         last_peak = peak
         last = number
         x += 1
